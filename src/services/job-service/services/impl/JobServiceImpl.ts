caf33debe89fed137cf8c6efd6fb6328eed8e152
arshakg-rgb <arshak.g@spiralsoft.co.il>
2026-08-17 17:52:50 +0400

    fix code

--- src/services/job-service/services/impl/JobServiceImpl.ts
+++ src/services/job-service/services/impl/JobServiceImpl.ts
@@ -191,6 +191,7 @@ export class JobServiceImpl implements JobService
     let s3Url: string | undefined;
     const uploadContentType: string = content_type || "application/octet-stream";
 
+
     if (source_type === SourceType.UPLOAD)
     {
       const rawName: string = (filename || "source").trim();
